d47b37cc6653873fc709426f581b06c61bd9c7d2
Anton Medvedev <anton.medvedev2@exness.com>
2025-05-20 15:57:07 +0300

    grid added

---
+++ public/background.js
@@ -0,0 +1,9 @@
+chrome.action.onClicked.addListener((tab) => {
+    chrome.tabs.create({ url: "index.html" });
+  });
+
+chrome.runtime.onInstalled.addListener((details) => {
+  if (details.reason === "install") {
+    chrome.tabs.create({ url: "welcome/index.html" });
+  }
+});
